5a5f820078a1964a1b2d1895d92106aeec29ea80
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-06-29 19:39:13 +0530

    feat: add Link to navigate

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,10 @@
+import { Link } from "react-router-dom";
+
 const HomePage = () => {
-  return <div>Home page</div>;
+  return (
+    <div>
+      Home page <Link to={"/product"}>Go to Product List</Link>
+    </div>
+  );
 };
 export default HomePage;
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,4 +1,10 @@
+import { Link } from "react-router-dom";
+
 const ProductPage = () => {
-  return <div>Products</div>;
+  return (
+    <div>
+      Products<Link to={"/"}>Back to Home</Link>
+    </div>
+  );
 };
 export default ProductPage;
