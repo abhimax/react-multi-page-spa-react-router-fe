3c86423e8ad4aa0740dfca61c63d2f8702431220
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-07-01 08:37:00 +0530

    feat: add useNavigate to nav dynamically

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,14 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const HomePage = () => {
-  return <div>Home page</div>;
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate("./products");
+  };
+  return (
+    <div>
+      Home page<button onClick={navigateHandler}>Navigate to Products</button>
+    </div>
+  );
 };
 export default HomePage;
