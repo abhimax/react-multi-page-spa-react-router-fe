39ba43127f6de5d160f2e05dec0c9420ea20aeac
Abhiman Ranaweera <abhiman.max@gmail.com>
2023-07-01 08:10:34 +0530

    feat: add error page

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductPage from "./pages/ProductPage";
 import RootLayout from "./pages/RootLayout";
+import ErrorPage from "./pages/ErrorPage";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductPage /> },
---
+++ src/pages/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import Navigation from "../components/Navigation";
+
+const ErrorPage = () => {
+  return (
+    <>
+      <Navigation />
+      <main>
+        <h1>Error Occurred!</h1>
+        <p>Could not find this page!</p>
+      </main>
+    </>
+  );
+};
+export default ErrorPage;
